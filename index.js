043140eb848a1d9cbc85826fd5b7e8f787042ccf
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-05-23 13:06:56 -0400

    Saving changes so far.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+require('dotenv').config()
+
+const port = process.env.PORT || 5000
+
+// process environment 
+
+// const express = require('express')
+
 const server = require('./api/server.js');
 
-server.listen(4000, () => {
+server.listen(port, () => {
   console.log('\n*** Server Running on http://localhost:4000 ***\n');
 });
